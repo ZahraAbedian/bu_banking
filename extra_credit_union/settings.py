add8dbf8c82c36b4e91fe921b38163fe4c8e232d
ZahraAbedian <zahraabediankhorasani@gmail.com>
2026-04-28 18:11:17 +0100

    feat: integrate python-dotenv to load environment variables from .env file

--- extra_credit_union/settings.py
+++ extra_credit_union/settings.py
@@ -11,10 +11,13 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+load_dotenv(BASE_DIR / ".env") 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
@@ -143,9 +146,6 @@ REST_FRAMEWORK = {
 CORS_ALLOW_ALL_ORIGINS = True  # For development only, don't use in production
 CORS_ALLOW_CREDENTIALS = True
 
-
-
-import os
 
 PAYMENT_NETWORK_BASE_URL = os.getenv(
     "PAYMENT_NETWORK_BASE_URL",
